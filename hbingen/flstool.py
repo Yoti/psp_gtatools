07b4e865c343afc66f922735c233e9cfb2b587e5
Yoti <realyoti@gmail.com>
2025-09-19 01:31:27 +0300

    [*] flstool v2025-09-18

--- hbingen/flstool.py
+++ hbingen/flstool.py
@@ -5,7 +5,7 @@ import os
 import sys
 import glob
 
-isoHashes = [
+isoHashesNoIntro = [
     ['9341cf2fb09dcee24c24d3830519bed6', 'Grand Theft Auto - Liberty City Stories (Europe) (v1.00).iso'],
     ['920642b8299f1655b23d638ce4ea6b56', 'Grand Theft Auto - Liberty City Stories (Europe) (v4.00).iso'],
     ['e6c1b891dd946d1c58157f281a23d2ed', 'Grand Theft Auto - Liberty City Stories (Germany).iso'],
@@ -14,6 +14,24 @@ isoHashes = [
     ['669783f1c7d3966385782ade672d2b98', 'Grand Theft Auto - Vice City Stories (Europe).iso'],
     ['8fdf7aa9bcbf0961750724781cc2a2c0', 'Grand Theft Auto - Vice City Stories (Germany).iso'],
     ['5b6b0ed024eaea63511a387efd6b9595', 'Grand Theft Auto - Vice City Stories (USA).iso'],
+]
+
+isoHashesRedump = [
+	['17cd20ebfae0c1d992a74f91234c96e6', 'Grand Theft Auto - Liberty City Stories (Europe) (En,Fr,De,Es,It) (v1.05).iso'],
+	['04c3e22762121ca59e8d6b83a0855211', 'Grand Theft Auto - Liberty City Stories (Europe) (En,Fr,De,Es,It) (v2.00).iso'],
+	['cf40e781475451db431769f9ea0f7998', 'Grand Theft Auto - Liberty City Stories (Europe) (En,Fr,De,Es,It) (v3.00).iso'],
+	['6ac9ad3b95e5ef96068d101c1b991bd7', 'Grand Theft Auto - Liberty City Stories (Germany) (v1.00).iso'],
+	['cf7947456ea763ecf8e330ad428a7f3d', 'Grand Theft Auto - Liberty City Stories (Germany) (v2.00).iso'],
+	['6deeae920efd43ca6967a8c6914be585', 'Grand Theft Auto - Liberty City Stories (Japan) (Rockstar Classics).iso'],
+	['00f1215e94c7a607d2d9d0ab3c1eeaaf', 'Grand Theft Auto - Liberty City Stories (Japan).iso'],
+	['c2733274c523d33431b1320ef516a1ee', 'Grand Theft Auto - Liberty City Stories (Korea) (En,Fr,De,Es,It).iso'],
+	['d08daddc86356f2cf5fdfa85cdcd0c91', 'Grand Theft Auto - Liberty City Stories (USA) (En,Fr,De,Es,It) (v1.05).iso'],
+	['7230bf9f55d89a34913f817b6e8a4fc4', 'Grand Theft Auto - Liberty City Stories (USA) (En,Fr,De,Es,It) (v3.00).iso'],
+	['bf17f0cabd2f3d9ba3c30413c070758e', 'Grand Theft Auto - Vice City Stories (Europe) (En,Fr,De,Es,It).iso'],
+	['ac8e2753bf955be62eb6338780b8367b', 'Grand Theft Auto - Vice City Stories (Germany).iso'],
+	['b5f6418d0ad7d1646a8cc67c56b805bd', 'Grand Theft Auto - Vice City Stories (Japan) (Rockstar Classics).iso'],
+	['6f2717cc3ced3a3646582533a3ce3ddc', 'Grand Theft Auto - Vice City Stories (Japan).iso'],
+	['f3734cd9ef8fa3f8daf07d0550481092', 'Grand Theft Auto - Vice City Stories (USA).iso'],
 ]
 
 headerSize = 2048
